8df8dac1e255a99be30222d4bec0277ff8da96f6
nogtk <taka.naoga@moneyforward.co.jp>
2023-07-04 16:24:54 +0900

    initial

---
+++ src/main/kotlin/com/example/dgs_sample/DgsSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dgs_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DgsSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<DgsSampleApplication>(*args)
+}
